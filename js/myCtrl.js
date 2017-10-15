cf5f5c4339d1c3cb4a4359d3a98de28dad7325c4
JennifferIng <jenniffering@outlook.com>
2017-10-15 00:04:41 -0500

    Añadiendo Angular/0 vistas

---
+++ js/myCtrl.js
@@ -0,0 +1,6 @@
+app.controller("myCtrl", function($scope) {
+    $scope.myCol ="lightblue";
+    $scope.firstName = "John";
+    $scope.lastName= "Doe";
+    $scope.names=['Jani','Hege','Kai'];
+});
